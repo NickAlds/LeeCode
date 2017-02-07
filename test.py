7454b842a5efe6e117b64ff340a3093865b2c025
NickAlds <aldslvda@live.com>
2017-02-07 10:14:16 +0800

    406. Queue Reconstruction by Height

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-list1 =  [[0 for col in range(5)] for row in range(5)]
-for i in range(5):
-    print list1[i]
+A= [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
 
-print '==============='
-#list1[1][1:4] = 1
-list1[1][1] = 1
-for i in range(5):
-    print list1[i]
+A1 = [[i[0],-i[1]] for i in A]
+A1.sort()
+
+print A1
+
+B = [i for i in range(len(A))]
+C = [0]*len(A)
+for i in A1:
+    #print i
+    C[B[-i[1]]] = [i[0],-i[1]]
+    B.pop(-i[1])
+print C
