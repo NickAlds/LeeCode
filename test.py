9a186d4895f8d6bcaf5cd2208446037515c025e1
NickAlds <aldslvda@live.com>
2017-02-09 16:10:09 +0800

    500. Keyboard Row

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-A= [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+A = [1,2,3]
+B = [1,2,3]
+str = 'aldslvda'
+print set(B)
+print set(A) == set(B)
+print set(str)
 
-A1 = [[i[0],-i[1]] for i in A]
-A1.sort()
-
-print A1
-
-B = [i for i in range(len(A))]
-C = [0]*len(A)
-for i in A1:
-    #print i
-    C[B[-i[1]]] = [i[0],-i[1]]
-    B.pop(-i[1])
-print C
