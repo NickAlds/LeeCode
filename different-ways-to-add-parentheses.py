81ac04261533781cc58481a53514737d33265529
NickAlds <aldslvda@live.com>
2017-03-08 17:23:14 +0800

    241. Different Ways to Add Parentheses

--- different-ways-to-add-parentheses.py
+++ different-ways-to-add-parentheses.py
@@ -7,7 +7,6 @@ Difficulty: Medium
 Contributors: Admin
 Given a string of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. The valid operators are +, - and *.
 
-
 Example 1
 Input: "2-1-1".
 
