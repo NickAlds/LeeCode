f67d923a4ea9554a6a79bfbc444922a1dfd4e013
NickAlds <aldslvda@live.com>
2019-03-29 09:53:44 +0800

    some changes

---
+++ cn/DivideConquer/242.valid-anagram.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+# https://leetcode.com/problems/valid-anagram/description/
+#
+# algorithms
+# Easy (51.30%)
+# Total Accepted:    314.1K
+# Total Submissions: 612K
+# Testcase Example:  '"anagram"\n"nagaram"'
+#
+# Given two strings s and t , write a function to determine if t is an anagram
+# of s.
+# 
+# Example 1:
+# 
+# 
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "rat", t = "car"
+# Output: false
+# 
+# 
+# Note:
+# You may assume the string contains only lowercase alphabets.
+# 
+# Follow up:
+# What if the inputs contain unicode characters? How would you adapt your
+# solution to such case?
+# 
+#
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        from collections import Counter
+        return Counter(s)==Counter(t)
+
---
+++ cn/DivideConquer/49.group-anagrams.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=49 lang=python3
+#
+# [49] Group Anagrams
+#
+# https://leetcode.com/problems/group-anagrams/description/
+#
+# algorithms
+# Medium (45.37%)
+# Total Accepted:    309.5K
+# Total Submissions: 681.4K
+# Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+#
+# Given an array of strings, group anagrams together.
+# 
+# Example:
+# 
+# 
+# Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+# Output:
+# [
+# ⁠ ["ate","eat","tea"],
+# ⁠ ["nat","tan"],
+# ⁠ ["bat"]
+# ]
+# 
+# Note:
+# 
+# 
+# All inputs will be in lowercase.
+# The order of your output does not matter.
+# 
+# 
+#
+class Solution:
+    def groupAnagrams(self, strs):
+        sorted_strs = [''.join(sorted(s)) for s in strs]
+        keys = set(sorted_strs)
+        anagrams_dict = {}
+        for i, s in enumerate(sorted_strs):
+            anagrams_dict[s] = anagrams_dict.get(s, [])+[strs[i]]
+        return list(anagrams_dict.values())
