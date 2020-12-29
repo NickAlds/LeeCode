92297fe269dd7d91a08f6e170da482cb067523c3
NickAlds <aldslvda@live.com>
2020-12-29 21:38:07 +0800

    LC02

--- 2.两数相加/add-two-numbers.py
+++ 2.两数相加/add-two-numbers.py
@@ -5,10 +5,23 @@ class ListNode:
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         carry = 0
-        sum_list = l1
-        while l1.next or l2.next:
-            l1.val = (l1.val + l2.val + carry)%10
-            carry = (l1.val + l2.val + carry)//10
-            l1 = l1.next
-            l2 = l2.next
+        head = ListNode(0)
+        cur = head
+        while l1 or l2:
+            if l1 and l2:
+                digit_sum = l1.val + l2.val + carry
+                l1, l2 = l1.next, l2.next
+            elif l1:
+                digit_sum = l1.val + carry
+                l1 = l1.next
+            else:
+                digit_sum = l2.val + carry
+                l2 = l2.next
+            carry = digit_sum//10
+            cur.next = ListNode(digit_sum%10)
+            cur = cur.next
+        if carry:
+            cur.next = ListNode(1)
+        return head.next
+
 
